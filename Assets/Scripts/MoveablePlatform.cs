d7f1af4128ad0a395bf0b6d772a1e32797b19dca
K_ar_el <verhulstkarel@hotmail.be>
2019-02-23 15:57:27 +0100

    moveable platforms
    prototyping some platform movement

---
+++ Assets/Scripts/MoveablePlatform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveablePlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] Waypoints;
+
+    [SerializeField]
+    private float _speed = 1;
+
+    private int _index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = Waypoints[_index].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlatformBetweenWaypoints();
+    }
+
+    private void MovePlatformBetweenWaypoints()
+    {
+        if (Vector3.Distance(transform.position, Waypoints[_index].position) <= _speed * Time.deltaTime)
+        {
+            ++_index;
+            _index %= Waypoints.Length;
+        }
+
+        this.transform.position = Vector3.MoveTowards(transform.position, Waypoints[_index].transform.position, _speed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/MoveablePlayerPlatform.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveablePlayerPlatform : MonoBehaviour
+{
+    private bool _moving;
+
+    private bool test;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_moving)
+        {
+            this.transform.position += Vector3.right * Time.deltaTime;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+        if (collision.gameObject.layer == 9)
+        {
+            _moving = true;
+            collision.transform.SetParent(this.transform);
+        }
+
+        if (collision.gameObject.tag == "stopPlatform")
+        {
+            _moving = false;
+            collision.transform.SetParent(null);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            _moving = false;
+            collision.transform.SetParent(null);
+        }
+    }
+}
