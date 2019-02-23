019f510144eebe391ab966ac704080f36602caa4
K_ar_el <verhulstkarel@hotmail.be>
2019-02-23 17:01:00 +0100

    wheel moving platform
    platform that rotates around a wheel

---
+++ Assets/Scripts/WheelPlatform.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private List<Transform> _platforms = new List<Transform>();
+
+    [SerializeField]
+    private float _speed = 1; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Vector3.forward * _speed * Time.deltaTime);
+
+        foreach (Transform item in _platforms)
+        {
+            item.transform.rotation = Quaternion.Euler(new Vector3(0,0, -this.transform.rotation.z));
+        }
+        
+    }
+}
