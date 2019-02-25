8faa962ca7a4b2781c878ae4c49c9ec8504e60af
K_ar_el <verhulstkarel@hotmail.be>
2019-02-25 19:17:17 +0100

    add cloud and lightning

---
+++ Assets/Scripts/CloudBehaviourEnemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBehaviourEnemy : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _lightningbolt;
+    [SerializeField]
+    private Transform _lightningPosition;
+
+    private SpriteRenderer _renderer;
+    private float _timer;
+    private bool _isShoot = false;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _timer += Time.deltaTime;
+
+        //todo color lerp
+        // shaking
+
+        
+
+        if (_timer >= 3 && _timer <= 7)
+        {
+            _renderer.color = Color.Lerp(Color.white, Color.gray, 1);
+           
+        }else if (_timer > 7 && _timer <= 10)
+        {
+            _renderer.color = Color.Lerp(Color.gray, Color.black, 1);
+            ShootLightningbolt();
+
+        }
+        else if (_timer > 10)
+        {
+            _isShoot = false;
+            _timer = 0;
+        }
+        else
+        {
+            _renderer.color = Color.Lerp(Color.black, Color.white, 1);
+        }
+        
+    }
+
+    private void ShootLightningbolt()
+    {
+        if (!_isShoot)
+        {
+            Instantiate(_lightningbolt, _lightningPosition.position, _lightningPosition.rotation);
+            _isShoot = true;
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/LightningBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningBehaviour : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+        _rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //_rb.AddForce(Vector3.down * 5);
+    }
+}
