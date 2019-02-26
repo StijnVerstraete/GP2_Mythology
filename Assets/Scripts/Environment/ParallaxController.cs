4b1c85eed564c9970e1a011b215f083e9c031feb
Stickie <pieterdestickere@gmail.com>
2019-02-26 01:21:00 +0100

    Proof of concept.

---
+++ Assets/Scripts/Environment/ParallaxController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+
+    #region Fields
+
+    //--- Private
+
+    // 
+
+    //--- Public
+    public GameObject[] ParallaxLayers;
+    public LayerMask BackgroundLayer;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Environment/ParallaxControllerEditor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[CustomEditor(typeof(ParallaxController))]
+public class ParallaxControllerEditor : Editor
+{
+    List<bool> _showFloat = new List<bool>();
+    ParallaxController _parallaxCon;
+
+    public override void OnInspectorGUI()
+    {
+
+        // Show default inspector property editor
+        DrawDefaultInspector();
+
+        _parallaxCon = (ParallaxController)target;
+
+        GUILayout.Label("Freeze layer when player idle:", EditorStyles.boldLabel);
+
+        if (_showFloat.Count == 0) FillList();
+        if (_showFloat.Count != _parallaxCon.ParallaxLayers.Length) RefreshList();
+
+        for (int i = 0; i < _parallaxCon.ParallaxLayers.Length; i++) HandleToggle(i); 
+
+    }
+
+    private void RefreshList()
+    {
+        List<bool> _showFloatTemp = _showFloat;
+        _showFloat.Clear();
+        for (int i = 0; i < _parallaxCon.ParallaxLayers.Length; i++) AddValueToCollection(_showFloatTemp[i]);
+        Debug.Log("Refreshed list!");
+    }
+
+    private void FillList()
+    {
+        _showFloat.Clear();
+        for (int i = 0; i < _parallaxCon.ParallaxLayers.Length; i++) AddValueToCollection(false);
+        Debug.Log("Re-filled list!");
+    }
+
+    private void AddValueToCollection(bool value)
+    {
+        Debug.Log(string.Format("Added {0}", value));
+        _showFloat.Add(value);
+    }
+
+    private void HandleToggle(int i)
+    {
+        _showFloat[i] = EditorGUILayout.Toggle(string.Format("Layer {0}", i), _showFloat[i]);
+        if (!_showFloat[i]) _showFloat[i] = true;
+        else if (_showFloat[i]) _showFloat[i] = false;
+    }
+}
