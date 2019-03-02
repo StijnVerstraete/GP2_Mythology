6368efc112a278b5f63b03664cd9f86379dc72d7
K_ar_el <verhulstkarel@hotmail.be>
2019-03-02 18:18:10 +0100

    set up objects in level
    - use other testplayer script
    - make sure platforms works and player can jump while standing on a platform
    - add the enemies clouds that shoot lightning
    - lightning is destroy at a certain point
    - make some prefabs

--- Assets/Scripts/Player/TestPlayerController.cs
+++ Assets/Scripts/Player/TestPlayerController.cs
@@ -1,38 +1,27 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharController : MonoBehaviour
+public class TestPlayerController : MonoBehaviour
 {
-
-    #region Fields
     
-    //--- Private
     [SerializeField]
-    float _jumpForce = 400f;
+    private float _jumpForce = 1000f;
+    [SerializeField]
+    private float _maxSpeed = 3;
 
-    Rigidbody2D _rb;
-    float _move;
-    float _maxSpeed = 5;
+    private Rigidbody2D _rb;
+    private float _move;
     
-    bool _facingLeft, _grounded;
-    float _groundRadius = .1f;
 
-    //--- Public
-
-    public Transform GroundCheck;
-    public LayerMask GroundLayer;
-
-    #endregion
-
-    #region Methods
-
+    private bool _facingLeft;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         _rb = GetComponentInChildren<Rigidbody2D>();
-        
+
     }
 
     // Update is called once per frame
@@ -41,8 +30,8 @@ public class CharController : MonoBehaviour
         _move = Input.GetAxis("Horizontal");
 
         if (_move > 0 && _facingLeft || _move < 0 && !_facingLeft) Flip();
-
-        if (_grounded && Input.GetButtonDown("Jump")) Jump();
+        
+        if (Input.GetButtonDown("Jump")) Jump();
 
         if (Input.GetButtonDown("Action")) Action();
 
@@ -51,8 +40,7 @@ public class CharController : MonoBehaviour
     // Using FixedUpdate because physx
     private void FixedUpdate()
     {
-        _grounded = Physics2D.OverlapCircle(GroundCheck.position, _groundRadius, GroundLayer);
-        if (_grounded) _rb.velocity = new Vector2(_move * _maxSpeed, _rb.velocity.y);
+         _rb.velocity = new Vector2(_move * _maxSpeed, _rb.velocity.y);
     }
 
     private void Action()
@@ -62,7 +50,8 @@ public class CharController : MonoBehaviour
 
     private void Jump()
     {
-       _rb.AddForce(new Vector2(0, _jumpForce));
+        // _rb.AddForce(new Vector2(0, _jumpForce));
+        _rb.AddForce(Vector2.up * _jumpForce);
     }
 
     // Flips the player to represent turning.
@@ -72,26 +61,22 @@ public class CharController : MonoBehaviour
         Vector3 scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
-            
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.layer == 11)
+        if (collision.gameObject.layer == 11)
         {
-            Debug.Log(_grounded);
-            _grounded = true;
             this.transform.SetParent(collision.transform);
         }
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.name);
         if (collision.gameObject.layer == 11)
         {
             this.transform.SetParent(null);
         }
     }
-    #endregion
 }
---
+++ Assets/Scripts/RemoveRespawnObjects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveRespawnObjects : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //lightning bolt
+        if (other.gameObject.layer == 12)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
