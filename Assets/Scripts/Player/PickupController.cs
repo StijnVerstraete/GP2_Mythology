0a9e03c9708d55f11ab7bcd1086b3174acc6954a
Stickie <pieterdestickere@gmail.com>
2019-03-04 05:04:09 +0100

    Added Pickup Controller.

---
+++ Assets/Scripts/Player/PickupController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PickupController
+{
+    public static void Pickup(string _pickupType)
+    {
+        Debug.Log(_pickupType);
+    }
+}
