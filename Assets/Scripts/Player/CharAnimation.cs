581590fb61efdc38607da88fedc315d6f4071cf8
StijnVerstraete <43540802+StijnVerstraete@users.noreply.github.com>
2019-03-17 12:45:34 +0100

    Set Up Animation Code

--- Assets/Scripts/Player/CharAnimation.cs
+++ Assets/Scripts/Player/CharAnimation.cs
@@ -2,13 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerAnimations : MonoBehaviour
+public class CharAnimation : MonoBehaviour
 {
     [SerializeField] private Animator _anim;
+    [SerializeField] private GameObject _player;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //set IsRunning
+        if (_player.GetComponent<Rigidbody2D>().velocity.x != 0)
+        {
+            _anim.SetBool("IsRunning", true);
+            Debug.Log("IsRunning");
+        }
+        else
+        {
+            _anim.SetBool("IsRunning", false);
+        }
+        //set isJumping && isFalling
+        if (_player.GetComponent<Rigidbody2D>().velocity.y > 0)
+        {
+            _anim.SetBool("IsJumping", true);
+            Debug.Log("IsJumping");
+        }
+        else if (_player.GetComponent<Rigidbody2D>().velocity.y < 0)
+        {
+            _anim.SetBool("IsFalling", true);
+            _anim.SetBool("IsJumping", false);
+            Debug.Log("IsFalilng");
+        }
+        else
+        {
+            _anim.SetBool("IsFalling", false);
+        }
     }
 }
