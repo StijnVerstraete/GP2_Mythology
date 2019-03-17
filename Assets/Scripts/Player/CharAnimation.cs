474a329cfe15bc9586c7e5634f68fd2927f7c514
StijnVerstraete <43540802+StijnVerstraete@users.noreply.github.com>
2019-03-17 12:29:23 +0100

    Animation Tree Setup

--- Assets/Scripts/Player/CharAnimation.cs
+++ Assets/Scripts/Player/CharAnimation.cs
@@ -1,14 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
-public class PlayerAnimations : MonoBehaviour
+public class CharAnimation : MonoBehaviour
 {
     [SerializeField] private Animator _anim;
+    [SerializeField] private GameObject _player;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //set falling
+        if (_player.GetComponent<Rigidbody2D>().velocity.y > 0.1f)
+        {
+            _anim.SetBool("IsJumping", true);
+            _anim.SetBool("IsFalling", false);
+        }
+        //set hanging
+        else if (!_player.GetComponent<PlayerController>().Ground.IsGrounded)
+        {
+            
+            _anim.SetBool("IsJumping", false);
+            _anim.SetBool("IsFalling", true);
+        }
+        //setrunning
+        else if(_player.GetComponent<Rigidbody2D>().velocity.x != 0)
+        {
+            _anim.SetBool("IsRunning", true);
+            _anim.SetBool("IsFalling", false);
+        }
+        //setidling
+        else
+        {
+            _anim.SetBool("IsIdling",true);
+            _anim.SetBool("IsRunning", false);
+        }
     }
 }
