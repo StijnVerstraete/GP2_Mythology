492785bc40e6c222905a9b99dda435e5a9d2ac84
K_ar_el <verhulstkarel@hotmail.be>
2019-03-18 18:53:26 +0100

    add test file for lightning

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log(other.name);
+    }
+}
