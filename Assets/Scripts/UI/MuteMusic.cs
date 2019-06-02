678e96665e19b59cfb45c0cd055d8c8ba60209ff
K_ar_el <verhulstkarel@hotmail.be>
2019-06-02 19:57:57 +0200

    edit music button

--- Assets/Scripts/UI/MuteMusic.cs
+++ Assets/Scripts/UI/MuteMusic.cs
@@ -1,38 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MenuBehaviour : MonoBehaviour
+public class MuteMusic : MonoBehaviour
 {
-    public GameObject MusicButton;
+    
     public Sprite MusicOnSprite;
     public Sprite MusicOffSprite;
 
     private bool _musicIsplaying = true;
     [SerializeField]
     private AudioSource _audio;
+    [SerializeField]
+    private Image _musicButton;
+    
 
     private void Start()
     {
         if (PlayerPrefs.GetInt("IsMusicPlaying") == 1)
         {
-            MusicButton.GetComponent<Image>().sprite = MusicOnSprite;
+            _musicButton.GetComponent<Image>().sprite = MusicOnSprite;
             _audio.Play();
         }
         else
         {
-            MusicButton.GetComponent<Image>().sprite = MusicOffSprite;
+            _musicButton.GetComponent<Image>().sprite = MusicOffSprite;
             _audio.Stop();
         }
     }
-
-    public void StartGame()
-    {
-        SceneManager.LoadScene(1);
-    }
-
+    
     public void MuteUnmuteSound()
     {
         //you can play or stop playing music
@@ -41,29 +38,14 @@ public class MenuBehaviour : MonoBehaviour
         if (_musicIsplaying)
         {
             PlayerPrefs.SetInt("IsMusicPlaying", 1);
-            MusicButton.GetComponent<Image>().sprite = MusicOnSprite;
+            _musicButton.GetComponent<Image>().sprite = MusicOnSprite;
             _audio.Play();
         }
         else
         {
             PlayerPrefs.SetInt("IsMusicPlaying", 0);
-            MusicButton.GetComponent<Image>().sprite = MusicOffSprite;
+            _musicButton.GetComponent<Image>().sprite = MusicOffSprite;
             _audio.Stop();
         }
-    }
-
-    public void ChangeCurrentPanel(GameObject currentPanel)
-    {
-        currentPanel.SetActive(false);
-    }
-
-    public void ChangeToNewPanel(GameObject newPanel)
-    {
-        newPanel.SetActive(true);
-    }
-
-    public void ExitGame()
-    {
-        Application.Quit();
     }
 }
